851018b4d94d5c37183c29c2cfeac48b7eb4678c
Tim Elfelt <timelf123@gmail.com>
2023-09-19 22:59:41 -0500

    migrations compile sql files as strings

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,8 +1,7 @@
 import { PoolClient } from "pg";
-
-import { readdir, readFile } from "./fs";
 import { WorkerSharedOptions } from "./interfaces";
 import { processSharedOptions } from "./lib";
+import * as sql from './sql';
 
 function checkPostgresVersion(versionString: string) {
   const version = parseInt(versionString, 10);
@@ -44,10 +43,7 @@ async function runMigration(
   migrationNumber: number,
 ) {
   const { escapedWorkerSchema } = processSharedOptions(options);
-  const rawText = await readFile(
-    `${__dirname}/../sql/${migrationFile}`,
-    "utf8",
-  );
+  const rawText = sql['sql_' + migrationFile.replace('.sql', '').replace(/-/g, '_')];
   const text = rawText.replace(
     /:GRAPHILE_WORKER_SCHEMA\b/g,
     escapedWorkerSchema,
@@ -92,7 +88,7 @@ export async function migrate(
     }
   }
 
-  const migrationFiles = (await readdir(`${__dirname}/../sql`))
+  const migrationFiles = Object.keys(sql).map(key => key.replace('sql_', '').replace(/_/g, '-') + '.sql')
     .filter((f) => f.match(/^[0-9]{6}\.sql$/))
     .sort();
   for (const migrationFile of migrationFiles) {
